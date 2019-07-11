90d5f7cfa71e9c1297639de5acd8c8637acbf5a8
Guilherme Sponda <sponda@archlinux.com.br>
2019-07-11 11:26:50 -0300

    fix(conflicts): fix merge conflicts in aws.py file

--- aws_dms_task_status_exporter/aws.py
+++ aws_dms_task_status_exporter/aws.py
@@ -1,23 +1,5 @@
 from aws_dms_task_status_exporter.helper import get_conf_file
 import boto3
-<<<<<<< HEAD
-from os import getenv
-
-
-cfg = get_conf_file()
-dms = boto3.client(
-    "dms",
-    aws_access_key_id=getenv("AWS_ACCESS_KEY", default=None),
-    aws_secret_access_key=getenv("AWS_SECRET_KEY", default=None),
-    region_name=getenv("AWS_REGION", default=cfg["region"]),
-)
-
-
-def get_status_replication_tasks():
-    result = []
-    response = dms.describe_replication_tasks(
-        Filters=[{"Name": "replication-task-id", "Values": cfg["replication-task-id"]}]
-=======
 from os import environ
 
 
@@ -35,7 +17,6 @@ def get_status_replication_tasks(dms_client=None):
     result = []
     response = dms_client.describe_replication_tasks(
         Filters=[{"Name": "replication-task-id", "Values": conf["replication-task-id"]}]
->>>>>>> 89d55f20c0b112c79aeaa47809ddb4fc04f69744
     )["ReplicationTasks"]
 
     for resp in response:
